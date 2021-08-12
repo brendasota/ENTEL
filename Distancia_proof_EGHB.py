e4025eed04d89a8b749d8e6550cbff875d597bde
EGholgado <erick.holgado@utec.edu.pe>
2021-08-11 22:35:58 -0500

    actualización

--- Distancia_proof_EGHB.py
+++ Distancia_proof_EGHB.py
@@ -1,7 +1,21 @@
-import numpy
 import math
+import numpy
 import pandas as pd
 from geopy import distance
+
+
+def dist_Haversine(lat1,lat2,long1,long2):
+    rad=numpy.pi/180
+    dlat=lat2-lat1
+    dlon=long2-long1
+    R=6372795.4775
+    a=(math.sin(rad*dlat/2))**2 + math.cos(rad*lat1)*math.cos(rad*lat2)*(math.sin(rad*dlon/2))**2
+    distancia=2*R*math.asin(math.sqrt(a))
+    return distancia
+
+def dist_geo(lat1,lat2,long1,long2):
+    distance=numpy.sqrt((lat1-lat2)**2+(long1-long2)**2)*100000
+    return distance
 
 def distancia1(lat1,lat2,long1,long2):
     coord_origen=(lat1,long1)
@@ -9,25 +23,14 @@ def distancia1(lat1,lat2,long1,long2):
     dist = distance.distance(coord_origen,coord_destino)
     return dist
 
-def distancia2(lat1,lat2,long1,long2):
-    distance=numpy.sqrt((lat1-lat2)**2+(long1-long2)**2)
-    return distance
-
-def distancia3(lat1,lat2,long1,long2):
-    rad=numpy.pi/180
-    dlat=lat2-lat1
-    dlon=long2-long1
-    R=6372.795477598
-    a=(math.sin(rad*dlat/2))**2 + math.cos(rad*lat1)*math.cos(rad*lat2)*(math.sin(rad*dlon/2))**2
-    distancia=2*R*math.asin(math.sqrt(a))
-    return distancia
 
 with open("train.csv",newline="") as Archivo:
     df = pd.read_csv(Archivo)
     for i in range(2):
-        print(distancia3(df.LATITUD_ORIGEN[i],df.LATITUD_DESTINO[i],df.LONGITUD_ORIGEN[i],df.LONGITUD_DESTINO[i]))
-        print(distancia1(df.LATITUD_ORIGEN[i],df.LATITUD_DESTINO[i],df.LONGITUD_ORIGEN[i],df.LONGITUD_DESTINO[i]))
-    print(distancia2(df.LATITUD_ORIGEN,df.LATITUD_DESTINO,df.LONGITUD_ORIGEN,df.LONGITUD_DESTINO))
-    print(df)
+        print(round(dist_Haversine(df.LATITUD_ORIGEN[i],df.LATITUD_DESTINO[i],df.LONGITUD_ORIGEN[i],df.LONGITUD_DESTINO[i]),1),end=" ")
+        print(round(dist_geo(df.LATITUD_ORIGEN[i],df.LATITUD_DESTINO[i],df.LONGITUD_ORIGEN[i],df.LONGITUD_DESTINO[i]),1),end=" ")
+        print(distancia1(df.LATITUD_ORIGEN[i],df.LATITUD_DESTINO[i],df.LONGITUD_ORIGEN[i],df.LONGITUD_DESTINO[i]),end=" ")
+        print(df.DISTANCIA[i])
+    
 
 
